203bbf3542505269851c610fd991a74ef0ff0205
Igor Mokrushin <i.mokrushin@ndmsystems.com>
2017-02-01 12:47:17 +0300

    initial commit backports 4.4.2 for linux 3.4.x

---
+++ backport-include/linux/firmware.h
@@ -0,0 +1,10 @@
+#ifndef __BACKPORT_LINUX_FIRMWARE_H
+#define __BACKPORT_LINUX_FIRMWARE_H
+#define CONFIG_FW_LOADER 1
+#include_next <linux/firmware.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,14,0)
+#define request_firmware_direct(fw, name, device) request_firmware(fw, name, device)
+#endif
+
+#endif /* __BACKPORT_LINUX_FIRMWARE_H */
